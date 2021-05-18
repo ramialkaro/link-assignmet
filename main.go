9d80b6c5d5c41ca7bb22c889fdf2c6fdac3fb661
ramialkaro <rami.alkaro@gmail.com>
2021-05-18 22:27:15 +0300

    main file restructuring.

--- main.go
+++ main.go
@@ -2,19 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	var xDevice, yDevice int
 
-	fmt.Println("hint, values should not be in string format, only numbers are accepted")
+	PrintLinkStationInfo()
+
+	var target TargetStruct
+	var device DeviceStruct
+
+	fmt.Println("\n\nhint, values should not be in string format, only numbers are accepted")
+
+	// read X value of the device from console.
 	fmt.Print("Enter X: ")
-	_, _ = fmt.Scanf("%d", &xDevice)
-	fmt.Print("Enter Y: ")
-	_, _ = fmt.Scanf("%d", &yDevice)
+	_, err := fmt.Scanf("%d", &device.x)
 
-	fmt.Printf("x: %v and y: %v\n", xDevice, yDevice)
-	stations := GetLinkStation()
-	GetResult(stations, xDevice, yDevice)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+
+		// read Y value of the device from console
+		fmt.Print("Enter Y: ")
+		_, err = fmt.Scanf("%d", &device.y)
+
+		if err != nil {
+			log.Fatal(err)
+		} else {
+
+			for _, station := range GetLinkStation() {
+
+				distance := CalculateDistance(device, station)
+
+				if distance > station.r {
+					if target.power == 0 {
+						target.power = 0
+					}
+				} else {
+					nextPower := CalculatePower(station.r, distance)
+					if target.power <= nextPower {
+						target.power = nextPower
+						target.x = station.x
+						target.y = station.y
+					}
+				}
+			}
+
+			if target.power != 0 {
+				color.Green("\nBest link station for point x:%d, y:%d is x:%d, y:%d with power %d\n",
+					device.x,
+					device.y,
+					target.x,
+					target.y,
+					target.power)
+
+			} else if target.power == 0 {
+				fmt.Printf("\nNo link station with reach for point x: %v, y: %v \n", device.x, device.y)
+			}
+		}
+	}
 
 }
