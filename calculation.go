250207f30c85eb4ca7c476fc4036cf0680480cd5
ramialkaro <rami.alkaro@gmail.com>
2021-05-18 22:24:12 +0300

    use struct for defining values and using them.

---
+++ calculation.go
@@ -0,0 +1,20 @@
+package main
+
+import "math"
+
+// calculate how far a device from link station
+func CalculateDistance(device DeviceStruct, station StationStruct) int {
+	var distance DistanceStruct
+
+	distance.x = (station.x - device.x)
+	distance.y = (station.y - device.y)
+	return int(
+		math.Sqrt(
+			float64(distance.x*distance.x) + float64(distance.y*distance.y)))
+}
+
+// Calculate the power function by give a reach and distance as arguments.
+func CalculatePower(reach int, distance int) int {
+	return (reach - distance) * (reach - distance)
+
+}
