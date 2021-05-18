6c4402aa8c3300f77238eb758b824931ea2a2f32
ramialkaro <rami.alkaro@gmail.com>
2021-05-18 22:23:10 +0300

    implement device, target, station, and distance srtucts

---
+++ Structs.go
@@ -0,0 +1,23 @@
+package main
+
+type DeviceStruct struct {
+	x int
+	y int
+}
+
+type TargetStruct struct {
+	x     int
+	y     int
+	power int
+}
+
+type StationStruct struct {
+	x int
+	y int
+	r int
+}
+
+type DistanceStruct struct {
+	x int
+	y int
+}
